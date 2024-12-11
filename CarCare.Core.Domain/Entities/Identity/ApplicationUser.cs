57c707447b7871f65927e447b821d87436c09c6b
Mahmoud Ahmed <Mahmoud Ahmed@DESKTOP-3CFC79A>
2024-12-11 21:02:56 +0200

    AddIdentityEntitiesMigration

---
+++ CarCare.Core.Domain/Entities/Identity/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCare.Core.Domain.Entities.Identity
+{
+	public class ApplicationUser : IdentityUser
+	{
+		public required DateTime BirthDate { get; set; }
+		public required string Address { get; set; }
+		public string? Specialization { get; set; }
+
+		[Length(14, 14)]
+		public string? CartNumber { get; set; }
+		public required string Name { get; set; }
+		public Type Type { get; set; }
+
+	}
+}
