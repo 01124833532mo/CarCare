108729099d470f05307ce2f8a9a9af01db80b381
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-01-25 03:30:12 +0200

    Edite User Name in identity to  allow same names

--- CarCare.Infrastructure.Persistence/_Data/Migrations/20250124235957_Make-UserName-NotUnique.Designer.cs
+++ CarCare.Infrastructure.Persistence/_Data/Migrations/20250124235957_Make-UserName-NotUnique.Designer.cs
@@ -4,6 +4,7 @@ using CarCare.Infrastructure.Persistence._Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarCare.Infrastructure.Persistence._Data.Migrations
 {
     [DbContext(typeof(CarCarIdentityDbContext))]
-    partial class CarCarIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250124235957_Make-UserName-NotUnique")]
+    partial class MakeUserNameNotUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -156,6 +159,8 @@ namespace CarCare.Infrastructure.Persistence._Data.Migrations
                         .HasFilter("[NormalizedUserName] IS NOT NULL");
 
                     b.HasIndex("ServiceId");
+
+                    b.HasIndex("UserName");
 
                     b.ToTable("AspNetUsers", (string)null);
                 });
